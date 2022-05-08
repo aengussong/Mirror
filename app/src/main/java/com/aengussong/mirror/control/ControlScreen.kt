0e970e594dbfda5f1a842ce2d24e4241188bc718
Oleg Durasov <oleg.durasov.2016@gmail.com>
2022-05-08 18:52:55 +0300

    [UI] add control screen (#5)
    Co-authored-by: oleh-durasov-hhl <oleh.durasov@hedgehoglab.com>

--- app/src/main/java/com/aengussong/mirror/control/ControlScreen.kt
+++ app/src/main/java/com/aengussong/mirror/control/ControlScreen.kt
@@ -1,10 +1,85 @@
 package com.aengussong.mirror.control
 
+import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.aengussong.mirror.R
 import com.aengussong.mirror.ui.navigation.Navigation
 
 @Composable
 fun ControlScreen(vm: ControlViewModel = viewModel(), onNavigate: (Navigation) -> Unit) {
+    val error by vm.errorFlow.collectAsState(initial = null)
+    LaunchedEffect(error) {
+        error?.let(onNavigate)
+    }
 
+    val data by vm.dataFlow.collectAsState()
+
+    LazyVerticalGrid(
+        modifier = Modifier.systemBarsPadding(),
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(8.dp)
+    ) {
+        items(data) { item ->
+            CrossfadeCard(item = item) {
+                vm.onControllerInteraction(it)
+            }
+        }
+    }
+}
+
+@Composable
+private fun CrossfadeCard(item: Controller, onItemClick: (Controller) -> Unit) {
+    Crossfade(targetState = item.isOn) { shouldShow ->
+
+        val imageTint =
+            if (shouldShow) MaterialTheme.colors.primary else MaterialTheme.colors.secondary
+        val cardTint =
+            if (!shouldShow) MaterialTheme.colors.primary else MaterialTheme.colors.secondary
+
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(1f)
+                .padding(4.dp)
+                .clickable { onItemClick(item) },
+            border = BorderStroke(15.dp, MaterialTheme.colors.secondary),
+            backgroundColor = cardTint
+        ) {
+            Icon(
+                modifier = Modifier.padding(20.dp),
+                painter = getPainterBy(item = item),
+                contentDescription = null,
+                tint = imageTint,
+            )
+        }
+    }
+}
+
+@Composable
+private fun getPainterBy(item: Controller): Painter {
+    return when (item.type) {
+        is Spook -> painterResource(id = R.drawable.ic_ghost)
+        is MessageForObserver -> painterResource(id = R.drawable.ic_walk)
+        is ShowTemperature -> painterResource(id = R.drawable.ic_thermostat)
+        is OpenRemoteControl -> painterResource(id = R.drawable.ic_web)
+        is Light -> painterResource(id = R.drawable.ic_lightbulb)
+        is Guests -> painterResource(id = R.drawable.ic_kabaddi)
+    }
 }
--- app/src/main/java/com/aengussong/mirror/control/ControlViewModel.kt
+++ app/src/main/java/com/aengussong/mirror/control/ControlViewModel.kt
@@ -1,6 +1,54 @@
 package com.aengussong.mirror.control
 
+import androidx.lifecycle.viewModelScope
 import com.aengussong.mirror.ui.base.BaseViewModel
+import com.aengussong.mirror.ui.navigation.Navigation
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class ControlViewModel : BaseViewModel() {
+
+    private val genericState = listOf(
+        Controller(Spook, false),
+        Controller(MessageForObserver, false),
+        Controller(ShowTemperature, false),
+        Controller(OpenRemoteControl, false),
+        Controller(Light, false),
+        Controller(Guests, false),
+    )
+
+    val dataFlow: StateFlow<List<Controller>> = MutableStateFlow(emptyList())
+
+    val errorFlow: SharedFlow<Navigation> = MutableSharedFlow()
+
+    init {
+        viewModelScope.launch {
+            dataFlow.emit(genericState)
+        }
+    }
+
+    fun onControllerInteraction(controller: Controller) {
+        viewModelScope.launch {
+            val data = dataFlow.value.takeIf { it.isNotEmpty() } ?: genericState
+            val index = data.indexOfFirst { it.type == controller.type }.takeIf { it >= 0 }
+                ?: throw IllegalStateException()
+            val updatedData = data.toMutableList().also {
+                it[index] = Controller(controller.type, !controller.isOn)
+            }
+            dataFlow.emit(updatedData)
+        }
+    }
 }
+
+class Controller(val type: ControllerType, val isOn: Boolean)
+
+sealed interface ControllerType
+object Spook : ControllerType
+object MessageForObserver : ControllerType
+object ShowTemperature : ControllerType
+object OpenRemoteControl : ControllerType
+object Light : ControllerType
+object Guests : ControllerType
